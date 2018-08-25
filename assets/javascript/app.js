388f6d4893c0b58b06ca2d46b7f61c5ebfd41903
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-08-24 23:41:42 -0700

    finished questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,182 @@
+const quizQuestions = [
+    {
+        question: "Round 1: Australia - What did Force India's Esteban Ocon achieve for the first time in his F1 career at Melbourne's season opener?",
+        answers: {
+            a: "Finish a Grand Prix",
+            b: "Score a point",
+            C: "Beat his team mate",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Round 2: China - Max Verstappen's third place for Red Bull ensured that three teams shared the Shanghai podium, but where did the Dutchman start?",
+        answers: {
+            a: "16th",
+            b: "10th",
+            c: "4th",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Round 3: Bahrain - Valtteri Bottas secured his first F1 pole position in Sakhir, but how many other Finns have qualified first for a Grand Prix?",
+        answers: {
+            a: "2",
+            b: "3",
+            C: "4",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Round 4: Russia - Haas's Romain Grosjean retired in spectacular fashion at the start in Sochi, after contact with who?",
+        answers: {
+            a: "Sauber's Marcus Ericsson",
+            b: "Toro Rosso's Daniil Kvyat",
+            C: "Renault's Jolyon Palmer"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Round 5: Spain - Which driver finished a season-best sixth in the Barcelona race?",
+        answers: {
+            a: "Force India's Esteban Ocon",
+            b: "Toro Rosso's Carlos Sainz",
+            c: "Renault's Nico Hulkenberg"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Round 6: Monaco - Ferrari's one-two in Monte Carlo was the Scuderia's first win in the Principality in how many years?",
+        answers: {
+            a: "6",
+            b: "10",
+            c: "16"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Round 7: Canada - Why was Toro Rosso's Daniil Kvyat immediately in trouble with the stewards at the beginning of the Montreal race?",
+        answers: {
+            a: "He caused a collision at the first corner",
+            b: "He jumped the start",
+            C: "He overtook cars illegally on the formation lap"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Round 8: Azerbaijan - The Baku race saw Williams' Lance Stroll become the youngest rookie to do what?",
+        answers: {
+            a: "Score a world championship point",
+            b: "Finish on the podium",
+            c: "Start from the front row of the grid"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Round 9: Austria - Valtteri Bottas won the Spielberg race by barely half a second, but who was the driver hounding him to the flag?",
+        answers: {
+            a: "Mercedes' Lewis Hamilton",
+            b: "Ferrari's Sebastian Vettel",
+            c: "Red Bull's Max Verstappen"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Round 10: Great Britain - Lewis Hamilton took his fifth British Grand Prix win in 2017, matching the all-time record for the race set by Alain Prost and which fellow Briton?",
+        answers: {
+            a: "Jim Clark",
+            b: "Graham Hill",
+            c: "Jackie Stewart"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Round 11: Hungary - Which driver set his (and his team's) only fastest lap of the year in Budapest?",
+        answers: {
+            a: "Force India's Sergio Perez",
+            b: "McLaren's Fernando Alonso",
+            c: "Red Bull's Daniel Ricciardo"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Round 12: Belgium - The second clash of the race between which team's two drivers brought out the safety car midway through the Spa round?",
+        answers: {
+            a: "Force India",
+            b: "Haas",
+            c: "Toro Rosso"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Round 13: Italy - Williams became one of only four teams to do what in 2017 in Monza?",
+        answers: {
+            a: "Win a Grand Prix",
+            b: "Qualify on pole position",
+            c: "Start from the front row of the grid"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Round 14: Singapore - Which two drivers recorded career-best results here (and one also learnt he was to lose his F1 seat to the other)?",
+        answers: {
+            a: "Carlos Sainz and Daniil Kvyat",
+            b: "Carlos Sainz and Jolyon Palmer",
+            c: "Daniil Kvyat and Jolyon Palmer"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Round 15: Malaysia - Sebastian Vettel infamously crashed into Lance Stroll's Williams on the Sepang slow-down lap, but which driver then gave the Ferrari star a lift back to the pits?",
+        answers: {
+            a: "Sauber's Pascal Wehrlein",
+            b: "Haas's Kevin Magnussen",
+            C: "Ferrari's Kimi Raikkonen"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Round 16: Japan - Haas brought both cars home in the points at Suzuka. How many times did the team achieve the feat in 2017?",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3"
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "Round 17: USA - New Zealander Brendon Hartley made his F1 race debut in Austin, but in which other motorsport series is he reigning world champion?",
+        answers: {
+            a: "WRC",
+            b: "WEC",
+            c: "MotoGP"
+        },
+        correctAnswer: "b"
+    },
+        {
+        question: "Round 18: Mexico - Lewis Hamilton wrapped up his fourth world title in Mexico City, but did not feature on the podium. Where did he finish the race?",
+        answers: {
+            a: "4th",
+            b: "6th",
+            c: "9th"
+        },
+        correctAnswer: "9th"
+    },
+    {
+        question: "Round 19: Brazil - Not three, but four race finishers appeared on the Sao Paulo podium: Sebastian Vettel, Valtteri Bottas, Kimi Raikkonen and who else?",
+        answers: {
+            a: "Lewis Hamilton",
+            b: "Max Verstappen",
+            c: "Felipe Massa"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Round 20: Abu Dhabi - With his second place at Yas Marina, Lewis Hamilton scored points for the 25th race in a row. Which current driver is the only man able to claim a longer scoring streak?",
+        answers: {
+            a: "Kimi Raikkonen",
+            b: "Sebastian Vettel",
+            c: "Fernando Alonso"
+        },
+        correctAnswer: "a"
+    },
+]
